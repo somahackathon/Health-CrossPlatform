03743006bd82b6b9a916bb1f9539c227df6c7a74
koreahghg <koreahghg@gmail.com>
2026-07-19 02:34:15 +0900

    feat: 데이터베이스 마이그레이션 및 프로필 스토어 로드 추가

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { migrate } from './src/db/client';
 import RootNavigator from './src/navigation/RootNavigator';
+import { useProfileStore } from './src/store/useProfileStore';
+
+migrate();
+useProfileStore.getState().load();
 
 export default function App() {
   return (
